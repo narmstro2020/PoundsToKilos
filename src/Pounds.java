da1e75e863564156424a442e57ddcd8e65a9eabf
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-08-07 13:49:00 -0400

    initial commit

---
+++ src/Pounds.java
@@ -0,0 +1,7 @@
+public class Pounds {
+    public static void main(String[] args) {
+        var pounds = 200;
+        var kilos = pounds * 0.45359237;
+        System.out.println("Kilograms: " + kilos);
+    }
+}
